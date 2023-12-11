e3b7caf74da1a53022ac4539179d3faa284e654c
jgl3hb <lewis.jeffreyg@gmail.com>
2023-12-11 10:45:29 -0500

    responses loading

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [conversation, setConversation] = useState([]);
+
+  const sendMessage = async () => {
+    const response = await fetch('http://localhost:3001/chat', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ message: input })
+    });
+    const data = await response.json();
+    setConversation([...conversation, { role: 'User', content: input }, { role: 'Oracle', content: data.reply }]);
+    setInput('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
+      <h1 className="text-2xl font-bold mb-4">GPT Oracle</h1>
+      <div className="flex w-full max-w-md">
+        <input
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          className="flex-1 p-2 border-2 border-gray-300 rounded-l"
+          placeholder="Type your message..."
+        />
+        <button onClick={sendMessage} className="bg-blue-500 text-white px-4 rounded-r">
+          Send
+        </button>
+      </div>
+      <div className="mb-4 flex flex-col items-center w-full max-w-md">
+        {conversation.map((msg, index) => (
+          <p key={index} className={`self-${msg.role === 'user' ? 'end' : 'start'} bg-${msg.role === 'user' ? 'blue-300' : 'green-300'} p-2 rounded my-1`}>
+            <b>{msg.role}:</b> {msg.content}
+          </p>
+        ))}
+      </div>
     </div>
   );
 }
+
 
 export default App;
