693a908b3553d2f82d1f93503987d3320f70c945
jgl3hb <lewis.jeffreyg@gmail.com>
2023-12-12 10:23:42 -0500

    tailwind

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4">
-      <h1 className="text-2xl font-bold mb-4">GPT Oracle</h1>
+      <h1 className="text-2xl font-bold mb-4">Oracle-GPT</h1>
       <div className="flex w-full max-w-md">
         <input
           value={input}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  // Remove the darkMode line if you don't need dark mode,
+  // or set it to 'media' or 'class' based on your preference
+  darkMode: 'media', // or 'class'
   theme: {
+    extend: {},
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
 }
-
